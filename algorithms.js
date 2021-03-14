c12855b50e5892a8d3084e5b30959ad567ec14e5
SpideyFanDan <dsmith.teach@gmail.com>
2021-03-13 18:02:48 -0700

    completed findMax logic and sorting logic

--- algorithms.js
+++ algorithms.js
@@ -63,14 +63,42 @@ function findMax(numbers) {
 // console.log(findMax([2, 3, 5, 9, 4, 16]));
 
 function sortBestRatingsFirst(numbers) {
-	let max_num = numbers[0];
-	let max_location;
-	for (let i = 0; i < numbers.length; i++) {
-		if (numbers[i] > max_num) {
-			max_num = numbers[i];
+	for (let j = 0; j < numbers.length - 1; j++) {
+		let max_num = numbers[j];
+		let max_location = j;
+		for (let i = j; i < numbers.length; i++) {
+			if (numbers[i] > max_num) {
+				max_num = numbers[i];
+				max_location = i;
+			}
+		}
+		numbers[max_location] = numbers[j];
+		numbers[j] = max_num;
+	}
+	return numbers;
+}
+// console.log(sortBestRatingsFirst([5, 8, 2, 9, 3, 10]));
+
+function findMaxHelper(numbers, start) {
+	let maximum = numbers[start];
+	let max_location = start;
+	for (let i = start; i < numbers.length; i++) {
+		if (numbers[i] > maximum) {
+			maximum = numbers[i];
 			max_location = i;
 		}
 	}
-	return [max_num, max_location];
+	return { max_number: maximum, max_index: max_location };
+}
+function sortBestWithHelper(numbers) {
+	for (let j = 0; j < numbers.length - 1; j++) {
+		max = findMaxHelper(numbers, j);
+		max_num = max['max_number'];
+		max_location = max['max_index'];
+
+		numbers[max_location] = numbers[j];
+		numbers[j] = max_num;
+	}
+	return numbers;
 }
 console.log(sortBestRatingsFirst([5, 8, 2, 9, 3, 10]));
